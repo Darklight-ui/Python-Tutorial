e33249c2c88233782bead83d863018bf06aa9bb3
Darklight-ui <henr9926@gmail.com>
2023-08-24 12:30:02 +0100

    Functions

---
+++ pythonTutorial/function.py
@@ -0,0 +1,52 @@
+# # no argument
+#
+# def say_my_name():
+#     print("Emmanuel")
+#     print("Taiye")
+#     print("Chisom")
+#
+#
+# # say_my_name()
+#
+# def say_my_name_2(name):
+#     print(name)
+#
+# # say_my_name_2("Emmanuel")
+#
+#
+# # Multiple argument
+# def greeting(greet, name):
+#     """
+#     greeting take in 2 arguments, greet & name
+#     :param greet:
+#     :param name:
+#     :return: >>> (greeting, user)
+#     """
+#     print(f"{greet}, {name}")
+#
+# # greeting("Holla!", "Emma")
+#
+# # default argument
+#
+# def greeting(name, greet='aloha'):
+#     print(f'{greet}, {name}')
+#
+# # greeting('Emmyboy')
+#
+# # positional argument
+# def greeting(name, greet='aloha'):
+#     print(f'{greet}, {name}')
+#
+# # greeting('Emmyboy')
+
+def sum (a, b):
+    """
+    Take 2 integers, return their sum
+    >>> sum(1, 2)
+    :return: 3
+    """
+    return a + b
+
+
+num = sum(1, 2)
+# print(num)
